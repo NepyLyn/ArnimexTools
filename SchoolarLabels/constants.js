aa59e4f0d1fcb1ab7dfcd73777f51b4c929cdada
Tonatew <rcrdroldan@gmail.com>
2022-08-21 17:37:26 -0500

    added .. to image path

--- SchoolarLabels/constants.js
+++ SchoolarLabels/constants.js
@@ -1,20 +1,20 @@
 let schoolLabels = [
     {id: 1, textSize: 0.25, lineHeight: "170%", textColor: "#9774B3",   textArea: [33.00, 23.23, 99.00, 76.66], paths:[
-        '/SchoolarLabels/Resources/unicornio-1.png',
-        '/SchoolarLabels/Resources/unicornio-2.png',
-        '/SchoolarLabels/Resources/unicornio-3.png',
-        '/SchoolarLabels/Resources/unicornio-4.png',
-        '/SchoolarLabels/Resources/unicornio-5.png',
-        '/SchoolarLabels/Resources/unicornio-6.png',
-        '/SchoolarLabels/Resources/unicornio-7.png',
-        '/SchoolarLabels/Resources/unicornio-8.png',
-        '/SchoolarLabels/Resources/unicornio-9.png',
-        '/SchoolarLabels/Resources/unicornio-10.png',
+        '../SchoolarLabels/Resources/unicornio-1.png',
+        '../SchoolarLabels/Resources/unicornio-2.png',
+        '../SchoolarLabels/Resources/unicornio-3.png',
+        '../SchoolarLabels/Resources/unicornio-4.png',
+        '../SchoolarLabels/Resources/unicornio-5.png',
+        '../SchoolarLabels/Resources/unicornio-6.png',
+        '../SchoolarLabels/Resources/unicornio-7.png',
+        '../SchoolarLabels/Resources/unicornio-8.png',
+        '../SchoolarLabels/Resources/unicornio-9.png',
+        '../SchoolarLabels/Resources/unicornio-10.png',
     ]},
     {id: 2, textSize: 0.28, lineHeight: "200%", textColor: "#0A0A0A",   textArea: [10.00, 10.00, 90.00, 90.00], paths:[
-        '/SchoolarLabels/Resources/clean-1.png',
+        '../SchoolarLabels/Resources/clean-1.png',
     ]},
     {id: 3, textSize: 0.28, lineHeight: "200%", textColor: "#33001a",   textArea: [12.00, 10.00, 90.00, 88.00], paths:[
-        '/SchoolarLabels/Resources/paint-1.png',
+        '../SchoolarLabels/Resources/paint-1.png',
     ]},
 ]
