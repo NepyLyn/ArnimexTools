a0fc676cd165da13789fe0fcf2a65e453b868520
Tonatew <rcrdroldan@gmail.com>
2022-08-28 17:08:20 -0500

    Added Pecil Labels

---
+++ pencilLabels/constants.js
@@ -0,0 +1,10 @@
+let margins = { //cm
+    page: 1,
+    x: 0.25,
+    y: 0.25
+  }
+  
+  let pageSize = {  //cm
+    width: 21.59,
+    height: 27.94
+  }
---
+++ pencilLabels/pencilLabels.js
@@ -0,0 +1,68 @@
+function calcLabelNumber(labelW, labelH){
+  useAreaWidth = pageSize.width - margins.page
+  useAreaHeight = pageSize.height - margins.page
+
+  columnsNumber = Math.floor(useAreaWidth/(parseInt(labelW) + margins.x))
+  rowsNumber = Math.floor(useAreaHeight/(parseInt(labelH) + margins.y))
+  let labelNumber = {cols: columnsNumber, rows:rowsNumber, total: columnsNumber*rowsNumber}
+  //console.log(columnsNumber, rowsNumber)
+  return labelNumber
+}
+
+function printLabelPage(){
+  let page = document.getElementById("labelPage");
+  
+  var opt = {
+    margin: 0.0001,
+    filename: "Etiquetas lapices.pdf",
+    image: { type: "png", quality: 1 },
+    html2canvas: { scale: 2, scrollY: 0, scrollX: -7},
+    jsPDF: { unit: "cm", format: "letter", orientation:  "portrait"},
+  };
+
+  // New Promise-based usage:
+  html2pdf().set(opt).from(page).save();
+}
+
+function fillPage(){
+  var page =  document.getElementById('labelPage');
+  page.innerHTML = ''; //Clear
+
+  let width = document.getElementById('inputWidth').value;
+  let height = document.getElementById('inputHeight').value;
+  let text = document.getElementById('inputText').value;
+  let labelsNumber = calcLabelNumber(width, height);
+  console.log(`W:${width} H:${height} T:${text} #:${labelsNumber.total}`);
+
+  var labelCount = 0
+  for (let i = 0; i < labelsNumber.total ; i++) {
+    var label = document.createElement('div')
+    label.classList.add('label-Instance', "text-dark")
+
+    label.innerHTML = text
+    label.style.width = width + "cm"
+    label.style.height = height + "cm"
+    label.style.marginRight = margins.x + "cm"
+    label.style.marginBottom = margins.y + "cm"
+    label.style.fontSize = height*0.8 + "cm"
+      
+    labelCount++;
+    page.appendChild(label)
+  }
+}
+
+function loadDesigns(){
+  labelContainer = document.getElementById('schoolLabels')
+  
+  schoolLabels.forEach(function(schoolLabel){
+    label = document.createElement('img')
+    label.src = schoolLabel.paths[0]
+    label.onclick = function() { fillPage(schoolLabel.id); };
+    label.classList.add('schoolLabel')
+    labelContainer.appendChild(label)
+  })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  
+});
